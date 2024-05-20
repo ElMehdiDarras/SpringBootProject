bffa4859b1df7eb6dad8a95df7f5fc62d468d676
ElMehdi Darras <elmehdidarras@gmail.com>
2024-05-20 14:51:11 +0100

    commit

---
+++ src/main/java/com/project/jee/jeedarras/JeedarrasApplication.java
@@ -0,0 +1,13 @@
+package com.project.jee.jeedarras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeedarrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeedarrasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/jee/jeedarras/JeedarrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jee.jeedarras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeedarrasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
